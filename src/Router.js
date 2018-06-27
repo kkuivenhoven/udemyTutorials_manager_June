635bb2c2f2ddcaede83e12ed6b1dfb32752ecfcb
Kendra Kuivenhoven <kendrakuivenhoven@Kendras-MacBook-Pro.local>
2018-06-27 07:47:53 -0700

    add button now works

--- src/Router.js
+++ src/Router.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Scene, Router } from 'react-native-router-flux';
+import { Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
 import EmployeeCreate from './components/EmployeeCreate';
