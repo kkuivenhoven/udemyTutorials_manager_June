0b369090e9e64c1f79412668d607aed49541d2ca
Kendra Kuivenhoven <kendrakuivenhoven@wlan-t-35-70-dhcp.CSUChico.EDU>
2018-06-27 11:04:24 -0700

    can now add new employee to database

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -14,6 +14,7 @@ export const employeeUpdate = ({ prop, value }) => {
 
 export const employeeCreate = ({ name, phone, shift }) => {
 	const { currentUser } = firebase.auth();
+	console.log(currentUser);
 	console.log(name, phone, shift);
 
 	return (dispatch) => {
