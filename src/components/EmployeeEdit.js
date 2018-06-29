ddd70d22e35b9a184dbec9b536fa40b116bd6283
Kendra Kuivenhoven <kendrakuivenhoven@Kendras-MacBook-Pro.local>
2018-06-29 16:07:41 -0700

    added text schedule button and appropriate react-native libraries

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Communications from 'react-native-communications';
 import EmployeeForm from './EmployeeForm';
 import { employeeUpdate, employeeSave } from '../actions';
 import { Card, CardSection, Button } from './common';
@@ -18,6 +19,12 @@ class EmployeeEdit extends Component {
 		this.props.employeeSave({ name, phone, shift, uid: this.props.employee.uid });
 	}
 
+	onTextPress(){
+		const { phone, shift } = this.props;
+
+		Communications.text(phone, `Your upcoming shift is on ${shift}`);
+	}
+
 	render() {
 		return (
 			<Card>
@@ -27,6 +34,13 @@ class EmployeeEdit extends Component {
 						Save Changes
 					</Button>
 				</CardSection>
+
+				<CardSection>
+					<Button onPress={this.onTextPress.bind(this)}>
+						Text Schedule
+					</Button>
+				</CardSection>
+			
 			</Card>
 		);
 	}
