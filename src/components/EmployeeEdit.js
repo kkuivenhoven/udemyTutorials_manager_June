4c61932f2d2815c5e577a4bb4294c36be6f46eee
Kendra Kuivenhoven <kendrakuivenhoven@Kendras-MacBook-Pro.local>
2018-06-29 17:30:40 -0700

    can now dismiss modal

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -4,10 +4,12 @@ import { connect } from 'react-redux';
 import Communications from 'react-native-communications';
 import EmployeeForm from './EmployeeForm';
 import { employeeUpdate, employeeSave } from '../actions';
-import { Card, CardSection, Button } from './common';
+import { Card, CardSection, Button, Confirm } from './common';
 
 
 class EmployeeEdit extends Component {
+	state = {showModal: false };
+
 	componentWillMount() {
 		_.each(this.props.employee, (value, prop) => {
 			this.props.employeeUpdate({ prop, value });
@@ -21,9 +23,18 @@ class EmployeeEdit extends Component {
 
 	onTextPress(){
 		const { phone, shift } = this.props;
-
 		Communications.text(phone, `Your upcoming shift is on ${shift}`);
 	}
+
+
+	onAccept(){
+		
+	}
+
+	onDecline(){
+		this.setState({ showModal: false });
+	}
+	
 
 	render() {
 		return (
@@ -40,6 +51,20 @@ class EmployeeEdit extends Component {
 						Text Schedule
 					</Button>
 				</CardSection>
+
+				<CardSection>
+					<Button onPress={() => this.setState({ showModal: !this.state.showModal })}>
+						Fire Employee
+					</Button>
+				</CardSection>
+
+				<Confirm
+					visible={this.state.showModal}
+					onAccept={this.onAccept.bind(this)}
+					onDecline={this.onDecline.bind(this)}
+				>
+					Are you sure you want to delete this?
+				</Confirm>
 			
 			</Card>
 		);
